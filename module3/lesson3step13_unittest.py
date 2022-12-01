5143fb4026e908735aa8a669fd5945aa5ba8073b
rileyweed <robingood7554@gmail.com>
2022-12-01 18:16:19 +0300

    ADD firefox.ini, geckodriver.exe, readme.mg, requirements.txt FILE module3\lesson3step13_unittest.py INIT

---
+++ module3/lesson3step13_unittest.py
@@ -0,0 +1,46 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import configparser
+
+
+class TestUniqueSelectors(unittest.TestCase):
+
+    def setUp(self):
+        config = configparser.ConfigParser()
+        config.read("../firefox.ini")
+        self.browser = webdriver.Firefox(executable_path=config["firefox"]["firefox_driver_path"],
+                                         firefox_binary=config["firefox"]["firefox_exe_path"])
+
+    def tearDown(self):
+        self.browser.close()
+
+    def fill_form(self, link):
+        browser = self.browser
+        browser.implicitly_wait(5)
+        browser.get(link)
+
+        browser.find_element(By.CSS_SELECTOR, '.first_block .first').send_keys('Kea')
+        browser.find_element(By.CSS_SELECTOR, '.first_block .second').send_keys('Lisa')
+        browser.find_element(By.CSS_SELECTOR, '.third_class .third').send_keys('KL@google.com')
+
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+        welcome_text = browser.find_element(By.TAG_NAME, 'h1').text
+        return welcome_text
+
+    def test_registration1(self):
+        link = 'http://suninjuly.github.io/registration1.html'
+        registration_result = self.fill_form(link)
+
+        self.assertEqual("Congratulations! You have successfully registered!", registration_result)
+
+    def test_registration2(self):
+        link = 'http://suninjuly.github.io/registration2.html'
+        registration_result = self.fill_form(link)
+
+        self.assertEqual("Congratulations! You have successfully registered!", registration_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
