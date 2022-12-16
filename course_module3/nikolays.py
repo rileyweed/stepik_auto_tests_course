2f2209678d5bf8dc9337ed136d784da3a0e8dec1
rileyweed <robingood7554@gmail.com>
2022-12-16 13:26:09 +0300

    UPDATE nikolays.py

--- course_module3/nikolays.py
+++ course_module3/nikolays.py
@@ -6,16 +6,8 @@ import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
-
-def setUp():
-    config = configparser.ConfigParser()
-    config.read(f"{os.path.dirname(__file__)}\\..\\firefox.ini")
-    firefox_driver_path = f"{os.path.dirname(__file__)}\\..\\{config['firefox']['firefox_driver_path']}"
-
-    browser = webdriver.Firefox(executable_path=firefox_driver_path,
-                                     firefox_binary=config["firefox"]["firefox_exe_path"])
-    return browser
 
 def cry(browser):
     browser.get('http://boobooka.com/zvuki/zvuki-lyudej/zvuki-stonov/')
@@ -23,12 +15,11 @@ def cry(browser):
         "document.title = 'КОЛЯ ИДИ НАХУИ'; \
         document.getElementsByTagName('img')[0].src='https://i.ytimg.com/vi/xk9JRZZJJ2w/maxresdefault.jpg'; \
         document.getElementsByTagName('h1')[0].style.display='none'")
-    browser.find_element_by_css_selector("[aria-controls='mep_0'][title='Воспроизвести']").click()
+    browser.find_element(By.CSS_SELECTOR, "[aria-controls='mep_0'][title='Воспроизвести']").click()
     time.sleep(20)
 
 
-def main():
-    browser = setUp()
+def test_main(browser):
     try:
         browser.implicitly_wait(5)
         browser.get('https://i.ytimg.com/vi/xk9JRZZJJ2w/maxresdefault.jpg')
@@ -45,7 +36,3 @@ def main():
     finally:
         browser.quit()
         print('Shut up bitch!\nЭто конец!')
-
-
-if __name__ == '__main__':
-    main()
